8cf443445a20f157d05b07c98b609e3067c4a878
Thiago Urias Hirae <thiagourias_@hotmail.com>
2022-02-01 15:55:34 -0300

    Rotas funcionando

--- protfolio-thiago/app-routing.module.ts
+++ protfolio-thiago/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from 'src/app/components/about/about.component';
+import { ContactComponent } from 'src/app/components/contact/contact.component';
+
+const routes: Routes = [
+
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- protfolio-thiago/src/app/app.module.ts
+++ protfolio-thiago/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from '../../app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
